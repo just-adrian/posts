cbf2b93354ed7757df45b3b0b631388707138c55
just-adrian <grzelaadrian@hotmail.com>
2022-05-15 12:39:26 +0200

    initial commit

---
+++ be/posts.api/posts.dal/Post.cs
@@ -0,0 +1,11 @@
+﻿namespace posts.dal
+{
+    public class Post
+    {
+        public string Author { get; set; }
+
+        public string Title { get; set; }
+
+        public string Content { get; set; }
+    }
+}
