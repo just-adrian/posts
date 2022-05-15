e5224967ec6200f58392abc6119b139e63dff6fb
just-adrian <grzelaadrian@hotmail.com>
2022-05-15 23:43:30 +0200

    added components

--- fe/posts/src/app/app.module.ts
+++ fe/posts/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { ClarityModule } from '@clr/angular';
 import { AppComponent } from './app.component';
+import { PostListComponent } from './post-list/post-list.component';
+import { PostComponent } from './post/post.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PostListComponent,
+    PostComponent
   ],
   imports: [
     BrowserModule,
